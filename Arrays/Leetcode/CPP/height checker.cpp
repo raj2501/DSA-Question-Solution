963d4a0c0f4085b397c9bb1a03e7ed38a42e3a8a
praveen88p <praveenpatidarpatidar88@gmail.com>
2024-06-10 22:02:14 +0530

    Commit #7��

---
+++ Arrays/Leetcode/CPP/height checker.cpp
@@ -0,0 +1,21 @@
+// Que - Height Checker
+// Link - https://leetcode.com/problems/height-checker/
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> tmp = heights;
+        sort(tmp.begin(), tmp.end());
+        int retval = 0;
+        
+        for (int i = 0; i < tmp.size();i++)
+        {
+            if (tmp[i] != heights[i])
+            {
+                retval++;
+            }
+        }
+        
+        return retval;
+    }
+};
